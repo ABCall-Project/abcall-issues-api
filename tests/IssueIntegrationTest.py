e4308ee221d8ccd39705a7e02105dce4e14115be
fabioCastagnola <f.castagnola@uniandes.edu.co>
2024-11-19 00:04:22 -0500

    Update IssueIntegrationTest.py

--- tests/IssueIntegrationTest.py
+++ tests/IssueIntegrationTest.py
@@ -154,7 +154,7 @@ class IssueIntegrationTest(unittest.TestCase):
                         .build()
         issues = []
         issues.append(expected_issue)
-        expected_response = FindIssueBuilder().with_data(issues).build()
+        expected_response = FindIssueBuilder().with_data(issues).with_has_next(False).build()
 
         self.client.post('/issue/post', content_type='multipart/form-data', data=data)
         response = self.client.get(f'/issue/getOpenIssues?page=1&limit=2')
